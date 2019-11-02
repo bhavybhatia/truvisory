b05d5f8175d5ceea536c49418dde99beddc72774
Bhavy Bhatia <bhatia.bhavy@gmail.com>
2019-11-02 16:00:39 +0530

    user model added

--- mutualfunds/models.py
+++ mutualfunds/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    name = models.TextField()
+    age = models.PositiveSmallIntegerField()
+    email = models.EmailField(max_length=254)
+    contact = models.BigIntegerField()
--- mutualfunds/views.py
+++ mutualfunds/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect,HttpResponse
+from .models import users
 
 def home(request):
     if request.method=='POST':
-        name = request.POST['name']
-        age = request.POST['age']
-        email = request.POST['email']
-        contact= request.POST['contact']
-        print(name,age,email,contact)
+        new_user=users(name=request.POST['name'], age = request.POST['age'], email = request.POST['email'], contact= request.POST['contact'])
+        new_user.save()
         return HttpResponse("Success")
     else:
         return render(request, "home.html")
