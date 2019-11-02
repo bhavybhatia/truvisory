74bee5527ac09811850be52b05603271742ee527
Bhavy Bhatia <bhatia.bhavy@gmail.com>
2019-11-03 02:20:53 +0530

    changed templates

--- mutualfunds/views.py
+++ mutualfunds/views.py
@@ -14,7 +14,6 @@ def portfolio(request):
     if request.method=='POST':
         add_income = income(sal_and_bonus=request.POST['sal_and_bonus'],sal_ends=request.POST['sal_ends'], exp_sal_growth_in_per=request.POST['exp_sal_growth_in_per'], rent=request.POST['rent'], rent_ends=request.POST['rent_ends'], exp_growth=request.POST['exp_growth'], business=request.POST['business'], business_ends=request.POST['business_ends'], business_growth=request.POST['business_growth'],
         other=request.POST['other'], other_ends=request.POST['other_ends'], other_growth=request.POST['other_growth'])
-
         add_income.save()
         return HttpResponse("Details Saved")
     else:
